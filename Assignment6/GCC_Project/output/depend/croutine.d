9f1f97c869ccaaea2d6f2a6d3100fe5b8c593a93
Neha Rani <ee20mtech14014@iith.ac.in>
2022-04-08 16:57:11 +0530

    commiting assignment 6

---
+++ Assignment6/GCC_Project/output/depend/croutine.d
@@ -0,0 +1,35 @@
+/home/sachin/Documents/Vaman/Assignment6/GCC_Project/output/croutine.o: \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/croutine.c \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/FreeRTOS.h \
+ /home/sachin/Documents/Vaman/Assignment6/inc/FreeRTOSConfig.h \
+ /home/sachin/Documents/Vaman/Assignment6/inc/Fw_global_config.h \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/projdefs.h \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/portable.h \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/deprecated_definitions.h \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/portable/GCC/ARM_CM4F_quicklogic_s3XX/portmacro.h \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/mpu_wrappers.h \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/task.h \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/list.h \
+ /home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/croutine.h
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/FreeRTOS.h:
+
+/home/sachin/Documents/Vaman/Assignment6/inc/FreeRTOSConfig.h:
+
+/home/sachin/Documents/Vaman/Assignment6/inc/Fw_global_config.h:
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/projdefs.h:
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/portable.h:
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/deprecated_definitions.h:
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/portable/GCC/ARM_CM4F_quicklogic_s3XX/portmacro.h:
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/mpu_wrappers.h:
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/task.h:
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/list.h:
+
+/home/sachin/Documents/Vaman/qorc-sdk//FreeRTOS/include/croutine.h:
